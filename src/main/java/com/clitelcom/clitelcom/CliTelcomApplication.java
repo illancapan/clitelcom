3ccb99fdcf8f80eb597e74b949696bc85c5e2371
illancapan <igorllancapan@gmail.com>
2024-12-11 18:44:23 -0300

    first commit

---
+++ src/main/java/com/clitelcom/clitelcom/CliTelcomApplication.java
@@ -0,0 +1,13 @@
+package com.clitelcom.clitelcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliTelcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CliTelcomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clitelcom/clitelcom/CliTelcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clitelcom.clitelcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliTelcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
